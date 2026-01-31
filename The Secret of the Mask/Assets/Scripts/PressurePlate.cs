97d004f696392a63645ea2e8b2d50f984762cb5f
Andrei Copaceanu <56719617+AndreiCopaceanu99@users.noreply.github.com>
2026-01-31 11:08:17 +0000

    Pressure plate gets activated

--- The Secret of the Mask/Assets/Scripts/PressurePlate.cs
+++ The Secret of the Mask/Assets/Scripts/PressurePlate.cs
@@ -18,11 +18,12 @@ public class PressurePlate : MonoBehaviour
     [SerializeField] LayerMask maskLayer;
 
     bool enabled;
+    SpriteRenderer spriteRenderer;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
@@ -30,24 +31,48 @@ public class PressurePlate : MonoBehaviour
     {
         if (!enabled)
         {
-            RaycastHit hit;
-            if (Physics.Raycast(transform.position, transform.up, out hit, 0.5f, maskLayer))
+            /*RaycastHit hit;
+            if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, 2f, maskLayer))
             {
                 Debug.Log("Pressure plate activated by " + hit.collider.name);
                 // Add logic for when the pressure plate is activated
                 CheckMask(hit.transform.GetComponent<MaskCubesSprites>().maskType);
-            }
+            }*/
         }
     }
 
     void CheckMask(MaskCubesSprites.MaskType mask)
     {
-
+        if(maskType.ToString() == mask.ToString())
+        {
+            spriteRenderer.color = Color.green;
+            enabled = true;
+        }
     }
 
     private void OnDrawGizmos()
     {
         Gizmos.color = Color.yellow;
-        Gizmos.DrawLine(transform.position, transform.position + new Vector3(0, 0.5f, 0));
+        Gizmos.DrawLine(transform.position, transform.position + new Vector3(0, 2f, 0));
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (!enabled && other.transform.tag == "Mask")
+        {
+            Debug.Log("Pressure plate activated by " + other.transform.name);
+            // Add logic for when the pressure plate is activated
+            CheckMask(other.transform.GetComponent<MaskCubesSprites>().maskType);
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (enabled && other.transform.tag == "Mask")
+        {
+            spriteRenderer.color = Color.white;
+            enabled = false;
+        }
+            // Optional: Logic for when the object leaves the pressure plate
     }
 }
